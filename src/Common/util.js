cb8648f03efdfc5b77995811dbff88ccc58e1591
kinneraMannava <mannava.kinnera67@gmail.com>
2020-11-13 21:02:00 +0100

    Add files via upload

---
+++ src/Common/util.js
@@ -0,0 +1,6 @@
+export const TimeMenu = ['1 Day','7 Days', '1 Month'];
+export const HaltsMenu = ['1 Halt', '<=4 Halts', '>4 Halts'];
+export const PeopleMenu = ['1 Person', '3 Persons', '5 Persons'];
+export const studentMenu = ['Yes', 'No'];
+export const Labels = [{'id': 1, 'value': 'Expected Days of Trip'}, {'id': 2, 'value': 'Number of persons'}, {'id': 3, 'value': 'Expected Number of Halts'}];
+export const temp = "Based on the information you have selected our suggestion of Ticket is:"
